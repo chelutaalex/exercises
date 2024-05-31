795741f3b3ef6e855a48c8f8243894bad80f9dca
chelutaalex <chelutaandrei@gmail.com>
2024-05-31 14:25:43 +0200

    esercizio 4

--- esercizijava/index.js
+++ esercizijava/index.js
@@ -1,16 +1,27 @@
-//esercizio 1 e 2 javascript
-const variabile2="Mondo";
+/*esercizio 1 e 2 javascript
+const variabile2 = "Mondo";
 
 variabile1 = "Ciao" ;
 console.log(variabile1);
 console.log(variabile2);
 
 //esercizio 3 
-let number=5; 
+let number = 5; 
 {
-    let number=14;
+    let number = 14;
     console.log(number);
 }
 
 console.log(number);
 
+*/
+
+//esercizio 4 
+let outside = 10;
+{
+    let inside = 20;
+    console.log(outside);
+    console.log(inside);
+}
+console.log(outside);
+console.log(inside);
