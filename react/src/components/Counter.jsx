d9caafe805b7734148fe02bdc77bfcc34c5e4faa
chelutaalex <chelutaandrei@gmail.com>
2024-07-24 10:29:52 +0200

    handledecrement

--- react/src/components/Counter.jsx
+++ react/src/components/Counter.jsx
@@ -5,15 +5,17 @@ const [count,setCount] = useState(0);
 const handleIncrement = () => {
     setCount(count+1)
 }
+const handleDecrement = () => {
+    setCount(count-1)
+}
 
     return (
         <>
         <div>
             Count:{count}
         </div>
-
-        <button onClick={handleIncrement}>Incrementa</button>
-
+            <button onClick={handleIncrement}>Incrementa</button>
+            <button onClick={handleDecrement}>Decrement</button>
         </>
     )
         
