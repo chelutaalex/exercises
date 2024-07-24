d640ecefd4dc55eb586df124d37a59f73b835a93
chelutaalex <chelutaandrei@gmail.com>
2024-07-24 10:27:50 +0200

    handleincrement

--- react/src/components/Counter.jsx
+++ react/src/components/Counter.jsx
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react"
 
 const Counter = () => {
 const [count,setCount] = useState(0);
+const handleIncrement = () => {
+    setCount(count+1)
+}
 
     return (
+        <>
         <div>
             Count:{count}
         </div>
-    )
 
+        <button onClick={handleIncrement}>Incrementa</button>
+
+        </>
+    )
+        
 } 
 
 export default Counter
