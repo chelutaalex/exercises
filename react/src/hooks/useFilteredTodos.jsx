55b7027a3f2a348bc21323f857cbd9eb1c2f4606
chelutaalex <chelutaandrei@gmail.com>
2024-08-20 22:55:24 +0200

    usefilteredtodos

---
+++ react/src/hooks/useFilteredTodos.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+function useFilteredTodos(todos, searchTerm) {
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    const filtered = todos.filter(todo =>
+      todo.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredTodos(filtered);
+  }, [todos, searchTerm]);
+
+  return filteredTodos;
+}
+
+export default useFilteredTodos;
