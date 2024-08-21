30c7cbc58de2851a1ded877321a5aeefd679ff37
chelutaalex <chelutaandrei@gmail.com>
2024-08-21 10:30:22 +0200

    aggiungere funzione todo

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,18 @@
 import {Todo} from "./types"
 
 let todos: Todo[] = [];
+
+function addTodo(title:string) {
+    
+    const todo:Todo= {
+        id:todos.length + 1,
+        title:title,
+        completed:false
+    }
+    
+    todos.push(todo)
+}
+
+addTodo("lavare i piatti")
+addTodo("Portare fuori il cane")
+console.log(todos)
