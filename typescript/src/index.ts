62bbbcfefdd2c215185492ded37f1eebbd87c2f2
chelutaalex <chelutaandrei@gmail.com>
2024-08-23 13:02:40 +0200

    associare todo con utenti

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,12 @@
 import {Todo} from "./types"
+import {User} from "./types"
 
 let todos: Todo[] = [];
+
+let users: User[] = [
+    { id: 1, name: 'Marco' },
+    { id: 2, name: 'Luca' }
+];
 
 function addTodo(title:string) {
     
@@ -13,6 +19,20 @@ function addTodo(title:string) {
     todos.push(todo)
 }
 
+function assignTodoToUser(todoId: number, userId: number): void {
+    const todo = todos.find(t => t.id === todoId);
+    
+    if (todo) {
+        todo.userId = userId;
+        console.log(`Utente:${userId} assegnato a todo: ${todoId}`);
+    } else {
+        console.error(`L'id:${todoId} non esiste`);
+    }
+}
+
 addTodo("lavare i piatti")
 addTodo("Portare fuori il cane")
+
+assignTodoToUser(1,2)
+
 console.log(todos)
--- typescript/src/types.ts
+++ typescript/src/types.ts
@@ -1,11 +1,12 @@
 export interface Todo {
     id:number,
     title:string,
-    completed:boolean
+    completed:boolean,
+    userId?:number
 }
 
 export interface User {
     id:number,
     name:string,
-    email:string
+    email?:string
 } 
