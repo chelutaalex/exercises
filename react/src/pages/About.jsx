bd46f7729e325aaae5ef827f902b40171b153fa9
chelutaalex <chelutaandrei@gmail.com>
2024-08-29 12:24:05 +0200

    usenavigate

--- react/src/pages/About.jsx
+++ react/src/pages/About.jsx
@@ -1,12 +1,19 @@
-
+import { useNavigate } from 'react-router-dom';
 
 const About = () => {
-    return(
-        <>
-            <h1>About</h1>
-            <p>Questa è un app creata per esercitarsi con React</p>
-        </>
-    )
-}
+  const navigate = useNavigate();
 
-export default About
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <>
+      <h1>About</h1>
+      <p>Questa è un'app creata per esercitarsi con React</p>
+      <button onClick={handleGoHome}>Vai alla Home</button>
+    </>
+  );
+};
+
+export default About;
