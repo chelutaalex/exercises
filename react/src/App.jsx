9f770a75338a40c52d0c87a8d2e535720592a953
chelutaalex <chelutaandrei@gmail.com>
2024-08-29 12:18:12 +0200

    outlet

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -12,18 +12,20 @@ import { Route } from "react-router-dom"
 import Home from './pages/Home.jsx'
 import About from "./pages/About.jsx"
 import Navbar from './components/Navbar.jsx'
+import Layout from "./components/Layout.jsx"
 
 function App() {
 
   return (
-          <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} /> 
-                <Route path="/about" element={<About />} />
-            </Routes>
-          </>
-        );
+    <TodoProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </TodoProvider>
+  );
 }
 
 export default App
