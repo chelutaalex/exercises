01dc014155386d8450349675a7e80db69da0b810
chelutaalex <chelutaandrei@gmail.com>
2024-08-30 14:16:40 +0200

    abilita dark mode

--- tailwind/index.js
+++ tailwind/index.js
@@ -1,6 +1,6 @@
 const toggleDarkMode = () => {
-    document.body.classList.toggle("dark");
-    document.querySelector("#darkNodeStatus").innerHTML = document.body.classList.contains("dark") ? "Active" : "Inactive";
+    document.documentElement.classList.toggle("dark");
+    document.querySelector("#darkModeStatus").innerHTML = document.body.classList.contains("dark") ? "Active" : "Inactive";
   };
   
   document.querySelector("#darkModeToggle").addEventListener("click", toggleDarkMode);
